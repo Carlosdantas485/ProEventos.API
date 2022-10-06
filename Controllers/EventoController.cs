bfc91bda8afa462b79d5fa3c1af8fdce4965ef3c
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-10-06 14:50:12 -0300

    Crate Api (C.R.U.D)

---
+++ Controllers/EventoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+           return "Get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+           return "Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+           return "Post";
+        }
+        
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+           return "Post";
+        }
+        
+    }
+}
